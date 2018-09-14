803843e69e1baf9d5f908478a115f35e7c617ed9
Peter Bourgon <peter@bourgon.org>
2018-09-14 08:59:04 -0700

    Duplicate detection

--- addimage.fish
+++ addimage.fish
@@ -20,14 +20,21 @@ while true
 	end
 end
 
+set hash (shasum --binary --algorithm 256 $image | awk '{print $1}')
+for f in (find img -type f)
+	if test $hash = (shasum --binary --algorithm 256 $f | awk '{print $1}')
+		echo "duplicate: $f"
+		exit
+	end
+end
+
 set extension (echo $image | string split '.' | tail -n1)
-set lastfile (ls -c1 img/ | tail -n1)
+set lastfile (ls -c1 img | sort | tail -n1)
 set laststr (echo $lastfile | string split '.' | head -n1)
-set nextint (math $laststr + 1)
-set nextstr (printf '%04d' $nextint)
+set nextstr (printf '%04d' (math $laststr + 1))
 set nextfile {$nextstr}.{$extension}
 
 cp $image img/$nextfile
-git add img/$nextfile
-git commit -m $nextstr
-git push origin master
+echo git add img/$nextfile
+echo git commit -m $nextstr
+echo git push origin master
